b8dbe8b2f2a3da11ceaa0035f4587886ed32f4a6
Zane Wang <cup@ZanedeMacBook-Air.local>
2025-10-16 20:13:19 +1100

    reslove conflict

--- app/routes.ts
+++ app/routes.ts
@@ -10,6 +10,6 @@ export default [
     route("signup", "./routes/signup.tsx"),
     route("profile", "./routes/profile.tsx"),
     route("business-registration", "./routes/business-registration.tsx"),// 末尾加上提示收到邮箱验证 查看邮箱做验证 验证之后可能再审核之类
-    route("customer-registration", "./routes/customer-registration.tsx"),
+    route("customer-registration", "./routes/customer-registration.tsx"),// 分开 customer和business
     route("*", "./routes/404.tsx"),
 ] satisfies RouteConfig;
